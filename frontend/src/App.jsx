acf26df4909c338d0fec78b71c11488ea1c669a9
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-06-08 22:17:33 +0530

    added login and register page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+// import Register from './pages/Register'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    
-    <>
-    <div>App</div>
-    </>
-    
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      
+    </Routes>
+    </BrowserRouter>    
   )
 }
 
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Login() {
+
+  return (
+    <div className='p-3 max-w-lg mx-auto mt-28'>
+    <h1 className='text-3xl text-center font-semibold my-7'>Login</h1>
+    <form className='flex flex-col gap-4 '>
+      <input
+        className='bg-slate-100 p-3 rounded-lg'
+        type="text"
+        placeholder='Username' />
+      
+      <input
+        className='bg-slate-100 p-3 rounded-lg'
+        type="password"
+        placeholder='Password'
+        id='password' />
+        <button
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+            Login
+        </button>
+    </form>
+    <div className='flex gap-2 mt-5'>
+      <p>Dont have an account?</p>
+      <Link to='/register'>
+        <span className='text-blue-500 '>Register</span>
+      </Link>
+    </div>
+    </div>
+  )
+}
+
+export default Login
